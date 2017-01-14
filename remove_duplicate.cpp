05f4cae437725b45f88fc3724f10bdef41fdedad
sherlcode13 <tirthprakashbal@gmail.com>
2017-01-14 23:26:58 +0530

    add remove duplicates from a string

---
+++ remove_duplicate.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+string remove_duplicates(string S){
+	int bv = 0;
+	for (int i = 0; i < S.length(); i++ ) {
+		int bp = S[i] - 'a';
+		if ( (bv >> bp) & 1) continue;
+		bv |= (1 << bp);
+	}
+	string tmp = "";
+	for (int cnt = 0; bv > 0; bv >>= 1, cnt++) {
+		if (!(bv & 1)) continue;
+		tmp += 'a' + cnt;
+	}
+	return tmp;
+}
+
+
+int main(){
+	string S;
+	cin>>S;
+	cout<<remove_duplicates(S)<<"\n";
+	return 0;
+}
