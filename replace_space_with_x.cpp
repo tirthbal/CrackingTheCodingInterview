344855a887024336046ea22a5df09113acc187f0
sherlcode13 <tirthprakashbal@gmail.com>
2017-01-15 23:41:56 +0530

    add some comments in replace_space_with_x

--- replace_space_with_x.cpp
+++ replace_space_with_x.cpp
@@ -1,6 +1,9 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// Stringstream is just a another stream buffer like iostream , fstream 
+// it is used mostly to parse the string inputted as get line
+
 vector<string> split(string S, char delim) {
 	stringstream ss;
 	ss.str(S);
@@ -24,9 +27,9 @@ string replace_space_with_x(string S, string x) {
 
 int main() {
 	string basic, out, S = "";
-	stringstream ss;
+	stringstream ss; 
 	getline(cin, basic);
-	ss<<basic;
+	ss<<basic; // ss.str(basic); both will set the content of basic in the stringstream buffer
 	while(ss>>out) { S += S == "" ? out : " " + out; }
 	cout<<S<<"\n";
 	cout<<replace_space_with_x(S, "%20")<<"\n";
