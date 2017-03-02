4bc0a78bb0194ab1cf969f80a6ea6cb4506bb3ca
sherlcode13 <tirthprakashbal@gmail.com>
2017-03-02 11:13:25 +0530

    fix logical errors in all_sequences_divisible_by_n code

--- all_sequences_divisble_by_n.cpp
+++ all_sequences_divisble_by_n.cpp
@@ -3,22 +3,36 @@ using namespace std;
 
 #define MAX_SIZE 1000
 
-int solve(string seq, int n) {
-	int dp[n + 2];
-	memset(dp, 0, n + 1);
-	for (int i = 0; i < seq.length(); i += 1) {
+int solve(char seq[], int n) {
+	int len  = strlen(seq);
+	int dp[len + 2][n + 2];
+
+	memset(dp, 0, sizeof(dp));
+	
+	dp[0][(int)(seq[0] - '0') % n]++;
+	for (int i = 1; i < len; i += 1) {
+		dp[i][(int)(seq[i] - '0') % n]++;
 		for (int j = 0; j < n; j += 1) {
-			int num = 10*j + (int)(seq[i] - '0');
-			dp[num % n] += 1 ;
+			int num = j*10 + (int)(seq[i] - '0');
+			dp[i][j] += dp[i-1][j];
+			if(!(num % n)) { cout<<num<<"\n";}
+			dp[i][num % n] += dp[i-1][j];
 		}
 	}
-	return dp[0];
+	for (int i = 0; i < len; i += 1){
+		for (int j = 0; j < n; j++){
+			cout<<dp[i][j]<<" ";
+		}
+		cout<<"\n";
+	}
+	return dp[len - 1][0];
 }
 
 int main() {
-	string seq;
+	char seq[1000];
 	int n;
-	cin>>seq>>n;
+	cin>>seq;
+	cin>>n;
 	cout<<solve(seq, n)<<"\n";
 	return 0;
 }
